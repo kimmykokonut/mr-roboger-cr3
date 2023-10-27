aa4cc23a9eb37d92c6e11039b008b06410b60dfe
Kim Robinson <kimmykokonut@gmail.com>
2023-10-27 09:33:25 -0700

    pass test 1 beepboop()

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// enter 0 get array
+
+function beepBoop(inputNumber) {
+  let counter = 0;
+  let beepArray = [];
+  if (beepArray.length <= inputNumber) {
+    beepArray.forEach(function(element) {
+      beepArray.push(startValue);
+      console.log(beepArray);
+      counter += 1;
+    });
+  return beepArray;
+  };
+}
