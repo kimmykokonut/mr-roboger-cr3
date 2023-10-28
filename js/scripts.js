961d824542ef5869f43503031884ca01767b51a4
Kim Robinson <kimmykokonut@gmail.com>
2023-10-27 17:03:38 -0700

    remove extranneous div tags

--- js/scripts.js
+++ js/scripts.js
@@ -41,40 +41,39 @@ function assessDigit(array) {
 function findReplace(array, findValue, replaceValue) {
   array[array.indexOf(findValue)] = replaceValue, array
   return array
-} 
+}
 
 function handleSelect(event) {
   event.preventDefault();
   const nameInput = document.getElementById("nameInput").value;
   const numberInput = parseInt(document.getElementById("numberInput").value);
-  let result = beepBoop(numberInput); 
+  let result = beepBoop(numberInput);
   const div = document.getElementById("results");
   const sayName = document.createElement("h4");
   sayName.append("Be my neighbor, " + nameInput + "?");
   div.append(sayName);
   const ul = document.createElement("ul");
   div.append(ul);
-  result.forEach(function(item) {
+  result.forEach(function (item) {
     let li = document.createElement("li");
     li.append(item);
     ul.append(li);
-  div.removeAttribute("class");
+    div.removeAttribute("class");
   });
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   let form = document.querySelector("form");
   let resetBtn = document.querySelector("button#reset");
-  // ? let results? to make input boxes null
   form.addEventListener("submit", handleSelect);
-  form.addEventListener("submit", function() {
+  form.addEventListener("submit", function () {
     resetBtn.removeAttribute("class");
   })
-  resetBtn.addEventListener("click", function() {
+  resetBtn.addEventListener("click", function () {
     divResults = document.querySelector("div#results");
     divResults.setAttribute("class", "hidden");
     document.getElementById("nameInput").value = null;
-    document.getElementById("numberInput").value = null;   
+    document.getElementById("numberInput").value = null;
     resetBtn.setAttribute("class", "hidden");
     window.location.reload(true);
   })
